fa00d274bc2e46e0e8b2d70f8c8f5d6e2097156d
cmtl0204 <cesar.tamayo0204@gmail.com>
2022-04-13 13:56:48 -0500

    add age

--- routes/api/v1/private/app.php
+++ routes/api/v1/private/app.php
@@ -22,18 +22,18 @@ Route::apiResource('catalogues', CatalogueController::class);
 /***********************************************************************************************************************
  * PATIENTS
  **********************************************************************************************************************/
-Route::controller(PatientController::class)->group(function () {
-    Route::prefix('patients/{patient}')->group(function () {
-        Route::post('clinical-histories','storeClinicalHistory');
-        Route::put('clinical-histories/{clinical_history}','updateClinicalHistory');
-        Route::get('clinical-histories','getClinicalHistories');
-        Route::put('users','updatePatientUser');
-    });
-
-    Route::prefix('patients')->group(function () {
-
-    });
-});
+//Route::controller(PatientController::class)->group(function () {
+//    Route::prefix('patients/{patient}')->group(function () {
+//        Route::post('clinical-histories','storeClinicalHistory');
+//        Route::put('clinical-histories/{clinical_history}','updateClinicalHistory');
+//        Route::get('clinical-histories','getClinicalHistories');
+//        Route::put('users','updatePatientUser');
+//    });
+//
+//    Route::prefix('patients')->group(function () {
+//
+//    });
+//});
 
 Route::apiResource('patients', PatientController::class);
 
--- routes/api/v1/public/app.php
+++ routes/api/v1/public/app.php
@@ -31,3 +31,16 @@ Route::controller(PatientController::class)->group(function () {
         Route::post('users','registerPatientUser');
     });
 });
+
+Route::controller(PatientController::class)->group(function () {
+    Route::prefix('patients/{patient}')->group(function () {
+        Route::post('clinical-histories','storeClinicalHistory');
+        Route::put('clinical-histories/{clinical_history}','updateClinicalHistory');
+        Route::get('clinical-histories','getClinicalHistories');
+        Route::put('users','updatePatientUser');
+    });
+
+    Route::prefix('patients')->group(function () {
+
+    });
+});
